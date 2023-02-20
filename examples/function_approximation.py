d38d927d1f97d3e008c0995acb3c02c284c39611
jloveric <john.loverich@gmail.com>
2023-02-19 19:32:03 -0800

    Plotting for rastrigin function

--- examples/function_approximation.py
+++ examples/function_approximation.py
@@ -1,5 +1,5 @@
 import torch
-
+from examples.function_approximation import rastrigin
 
 def rastrigin(x: torch.Tensor, A: float = 10):
     return A + sum((x**2 - A * torch.cos(2 * torch.pi * x)))
--- examples/plot_rastrigin.py
+++ examples/plot_rastrigin.py
@@ -3,21 +3,35 @@ Modified from this gist
 https://gist.github.com/miku/fca6afe05d65302f14c2b6f5242458d6
 """
 
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+https://en.wikipedia.org/wiki/Rastrigin_function
+Non-convex function for testing optimization algorithms.
+"""
+
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import math
 import matplotlib.pyplot as plt
-import torch
+import numpy as np
 
-if __name__ == "__main__":
-    X = torch.linspace(-4, 4, 200)
-    Y = torch.linspace(-4, 4, 200)
+def rastrigin(*X, **kwargs):
+    A = kwargs.get('A', 10)
+    return A + sum([(x**2 - A * np.cos(2 * math.pi * x)) for x in X])
 
-    X, Y = torch.meshgrid(X, Y)
+if __name__ == '__main__':
+    X = np.linspace(-4, 4, 200)    
+    Y = np.linspace(-4, 4, 200)    
+
+    X, Y = np.meshgrid(X, Y)
 
     Z = rastrigin(X, Y, A=10)
 
     fig = plt.figure()
-    ax = fig.gca(projection="3d")
+    ax = fig.add_subplot(projection='3d')
 
-    ax.plot_surface(
-        X, Y, Z, rstride=1, cstride=1, cmap=cm.plasma, linewidth=0, antialiased=False
-    )
-    plt.savefig("rastrigin.png")
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.plasma, linewidth=0, antialiased=False)  
+    plt.show()  
+    #plt.savefig('rastrigin.png')
